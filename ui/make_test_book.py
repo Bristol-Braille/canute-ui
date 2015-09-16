4aeb5547021e3d5e59cad516c65d21e234fcbce1
matt venn <matt@mattvenn.net>
2015-09-16 16:18:52 +0100

    debugging tool to generate books with same content as title

--- ui/make_test_book.py
+++ ui/make_test_book.py
@@ -1,4 +1,4 @@
-from utility import test_book
+from utility import test_book, alpha_to_pin_num
 from pageable import Library
 from ConfigParser import ConfigParser
 
@@ -14,3 +14,11 @@ with open(native_file, 'w') as fh:
     for page in pages:
         fh.write(bytearray(page))
 
+""" used for debugging wrong sort order
+# write 8 books with only one page
+for page in range(8):
+    native_file = book_dir + str(page) + Library.native_ext
+    print(native_file)
+    with open(native_file, 'w') as fh:
+        fh.write(bytearray([alpha_to_pin_num(str(page))]*dimensions[0]))
+"""
