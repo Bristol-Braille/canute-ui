fe403ad1665077cfe3a13ba3bec92ee242c3d0dc
matt venn <matt@mattvenn.net>
2015-09-16 18:41:42 +0100

    unittest framework started

--- ui/test.py
+++ ui/test.py
@@ -1,9 +1,31 @@
 import unittest
+from pageable import Menu, Book, Library
+from driver_emulated import Emulated
+from ConfigParser import ConfigParser
 
 class TestUtility(unittest.TestCase):
 
     def setUp(self):
         pass
+
+class TestPageable(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    @classmethod
+    def setUpClass(cls):
+        config = ConfigParser()
+        config.read('config.rc')
+
+        ui = None
+        content = []
+        dimensions = [20,4]
+        cls.menu = Menu(dimensions, config, ui)
+
+
+    def test_num_pages(self):
+        self.assertEqual(self.menu.get_num_pages(), 1)
 
 if __name__ == '__main__':
     unittest.main()
