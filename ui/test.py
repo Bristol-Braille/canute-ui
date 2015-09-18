bcee215be00c9fc86fe64c679c1a3fd9c24e4cff
matt venn <matt@mattvenn.net>
2015-09-18 12:16:15 +0100

    tests

--- ui/test.py
+++ ui/test.py
@@ -1,12 +1,56 @@
 import unittest
 from pageable import Menu, Book, Library
+from utility import *
 from driver_emulated import Emulated
 from ConfigParser import ConfigParser
+from bookfile_list import BookFile_List
+import os
 
 class TestUtility(unittest.TestCase):
 
     def setUp(self):
         pass
+
+    def test_pin_num_to_unicode(self):
+        for p in range(64):
+            self.assertEqual(unicode_to_pin_num(pin_num_to_unicode(p)), p)
+            
+    def test_pin_num_to_alpha(self):
+        for p in range(64):
+            self.assertEqual(alpha_to_pin_num(pin_num_to_alpha(p)), p)
+
+class TestBookFile_List(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls._book = 'book'
+        # create a test file
+        cls._dimensions = (28,4) #canute
+        pages = test_book(cls._dimensions)
+
+        with open(cls._book, 'w') as fh:
+            for page in pages:
+                fh.write(bytearray(page))
+
+        cls._bookfile = BookFile_List(cls._book, cls._dimensions)
+
+    def test_book_file_create(self):
+        self.assertIsInstance(self._bookfile, BookFile_List)
+
+    def test_book_file_len(self):
+        self.assertEqual(len(self._bookfile),8 * self._dimensions[1])
+
+    def test_book_file_content(self):
+        w = self._dimensions[0] 
+        h = self._dimensions[1]
+        # test every page of the book
+        for i in range(8):
+            expected_pin = i + (i << 3)
+            self.assertEqual(self._bookfile[i*h:i*h+1][0], (expected_pin,) * w)
+
+    @classmethod
+    def tearDownClass(cls):
+        os.unlink(cls._book)
 
 class TestPageable(unittest.TestCase):
 
