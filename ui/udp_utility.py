cda6b2b2047f4ee0451208f41988e119e0602e82
matt venn <matt@mattvenn.net>
2015-09-18 13:49:05 +0100

    pep8 and comments

--- ui/udp_utility.py
+++ ui/udp_utility.py
@@ -10,23 +10,32 @@ log = logging.getLogger(__name__)
 UDP_IP = "127.0.0.1"
 TIMEOUT = 0.5
 
-class udp_send():
 
+class udp_send():
+    """helper class for communications between emulated driver and GUI
+
+    :param port: port to send to
+    """
     def __init__(self, port=5000):
         self.port = port
-        self.sock = socket.socket(socket.AF_INET, # Internet
-                             socket.SOCK_DGRAM) # UDP
+        self.sock = socket.socket(socket.AF_INET,  # Internet
+                                 socket.SOCK_DGRAM)  # UDP
         log.debug("will send to port %d" % self.port)
 
-    def put(self,message):
+    def put(self, message):
         self.sock.sendto(pickle.dumps(message), (UDP_IP, self.port))
+
 
 class udp_recv():
 
+    """helper class for communications between emulated driver and GUI
+
+    :param port: port to listen on to
+    """
     def __init__(self, port=5000):
         self.port = port
-        self.sock = socket.socket(socket.AF_INET, # Internet
-                             socket.SOCK_DGRAM) # UDP
+        self.sock = socket.socket(socket.AF_INET,  # Internet
+                             socket.SOCK_DGRAM)  # UDP
         self.sock.setblocking(0)
         self.sock.bind((UDP_IP, self.port))
         log.debug("binding to port %d" % self.port)
@@ -53,10 +62,9 @@ if __name__ == '__main__':
 
     if args.send:
         u = udp_send(args.port)
-        msg = {"matt":"cool"}
+        msg = {"matt": "cool"}
         u.put(msg)
         log.info("sending %s" % msg)
     elif args.recv:
         u = udp_recv(args.port)
         log.info("got: %s" % u.get())
-
