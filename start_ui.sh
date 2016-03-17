5f4333ea795a48867d69504cc6c48b6ff9b5aa57
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-03-17 16:52:30 +0000

    Simplify start_ui and remove other scripts

--- start_ui.sh
+++ start_ui.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-pidfile=ui/ui.pid
-if [ -e $pidfile ]
-then
-    echo ui is already running
-    exit
-else
-    cd ui
-    sudo python ui.py --disable-emulator --pi-buttons &
-fi
+cd ui
+./ui.py --both
