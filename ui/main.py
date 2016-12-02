a41aa169bf223f7d940f0f59dbdde393d8140e10
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-02 15:02:41 +0000

    Implement page turning for library mode

--- ui/main.py
+++ ui/main.py
@@ -18,26 +18,39 @@ from ui import setup_logs
 HEIGHT = 9
 WIDTH = 40
 
-initial_state = {
-    'location'        : 'library',
-    'library'         : {'data': [], 'page': 0},
-    'books'           : [],
-    'button_bindings' : {}
-}
-
-action_types = ['set_books', 'go_to_book']
+action_types = ['set_books', 'go_to_book', 'next_page', 'previous_page']
 
 def make_action_method(name):
-    def f(value):
+    def action_method(value = None):
         return {'type': name, 'value': value}
-    return f
+    return action_method
 
 #just an empty object
 def actions(): pass
 
-#then we give it methods
+#then we give it a method for each action
 for action in action_types:
     setattr(actions, action, make_action_method(action))
+
+button_bindings = {
+    'library': {
+        'single': {
+            '>' : actions.next_page,
+            '<' : actions.previous_page,
+        }
+    }
+}
+
+initial_state = {
+    'location'        : 'library',
+    'library'         : {'data': [], 'page': 0},
+    'books'           : [],
+    'button_bindings' : button_bindings
+}
+
+
+def get_max_pages(data):
+    return len(data) // HEIGHT
 
 
 def update(state, action = None):
@@ -59,7 +72,24 @@ def update(state, action = None):
             )
         library = {'data': map(get_title, books), 'page': 0}
         return extend(state, {'books': books, 'library': library})
+    elif action['type'] == 'next_page':
+        if state['location'] == 'library':
+            data = state['library']['data']
+            page = state['library']['page'] + 1
+            if page > get_max_pages(data):
+                return state
+            else:
+                return extend(state, {'library': {'data': data, 'page': page}})
+    elif action['type'] == 'previous_page':
+        if state['location'] == 'library':
+            data = state['library']['data']
+            page = state['library']['page'] - 1
+            if page < 0:
+                return state
+            else:
+                return extend(state, {'library': {'data': data, 'page': page}})
     return state
+
 
 store = create_store(update, initial_state)
 
@@ -102,5 +132,11 @@ if __name__ == '__main__':
         file_names = utility.find_files(library_dir, '.canute')
         store.dispatch(actions.set_books(file_names))
         while not quit:
-            time.sleep(1)
+            buttons = driver.get_buttons()
+            state = store.get_state()
+            bindings = state['button_bindings']
+            location = state['location']
+            for id in buttons:
+                type = buttons[id]
+                store.dispatch(bindings[location][type][id]())
 
