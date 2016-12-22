b3bd7f31cbd87624b53a172977bb4eb882b50ce2
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-12-22 15:44:11 +0000

    Fix lack of conversion to float of timeout config

--- ui/driver_pi.py
+++ ui/driver_pi.py
@@ -71,7 +71,7 @@ class Pi(Driver):
         try:
             serial_port = serial.Serial()
             serial_port.port = port
-            serial_port.timeout = self.timeout
+            serial_port.timeout = float(self.timeout)
             serial_port.open()
             serial_port.flush()
             return serial_port
