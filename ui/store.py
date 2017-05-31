9b158034cc059ac4d55bb33a0ef084682b01a149
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-05-31 12:49:51 +0100

    Refactor reducer creation

--- ui/store.py
+++ ui/store.py
@@ -10,17 +10,25 @@ import utility
 from actions import Reducers
 import initial_state
 
-reducers = Reducers()
-reducer_dict = {}
-for name in dir(reducers):
-    if not name.startswith('__'):
-        reducer_dict[name] = getattr(reducers, name)
+def dictify(cls):
+    reducers = cls()
+    reducer_dict = {}
+    for name in dir(reducers):
+        if not name.startswith('__'):
+            reducer_dict[name] = getattr(reducers, name)
+    return reducer_dict
 
-def reducer(state, action = None):
-    log.debug(action)
-    for name in reducer_dict:
-        if action['type'] == name:
-            return reducer_dict[name](state, action['value'])
-    return state
+
+def makeReducer(cls):
+    reducer_dict = dictify(cls)
+    def reducer(state, action = None):
+        log.debug(action)
+        for name in reducer_dict:
+            if action['type'] == name:
+                return reducer_dict[name](state, action['value'])
+        return state
+    return reducer
+
+reducer = makeReducer(Reducers)
 
 store = pydux.create_store(reducer)
