9de54130c0172a567ad67b07095e3213f6083dcc
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-06-01 19:41:46 +0100

    Raise an error explicitly if config is empty

--- ui/config_loader.py
+++ ui/config_loader.py
@@ -6,7 +6,9 @@ config_file = 'config.rc'
 
 def load(config_file=config_file):
     config = ConfigParser()
-    config.read(config_file)
+    c = config.read(config_file)
+    if len(c) == 0:
+        raise ValueError('Please provide a config.rc')
     library_dir = config.get('files', 'library_dir')
     config.set('files', 'library_dir', os.path.expanduser(library_dir))
     if not config.has_section('comms'):
