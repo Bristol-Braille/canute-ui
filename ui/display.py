03a303c189d83a0fa2e9ec04cefbd0776b9cb0a6
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-06-02 22:29:20 +0100

    Refactor display module into class

--- ui/display.py
+++ ui/display.py
@@ -1,4 +1,3 @@
-
 import logging
 from .actions import get_max_pages, dimensions
 from . import utility
@@ -7,68 +6,63 @@ from . import utility
 log = logging.getLogger(__name__)
 
 
-def render(state):
-    width, height = dimensions(state['app'])
-    location = state['app']['location']
-    if location == 'library':
-        page = state['app']['library']['page']
-        data = state['app']['library']['data']
-        # subtract title from page height
-        data_height = height - 1
-        max_pages = get_max_pages(data, data_height)
-        n = page * data_height
-        data = data[n: n + data_height]
-        # pad page with empty rows
-        while len(data) < data_height:
-            data += ((0,) * width,)
-        title = format_title('library menu', width, page, max_pages)
-        set_display(tuple([title]) + tuple(data))
-    elif location == 'menu':
-        page = state['app']['menu']['page']
-        data = state['app']['menu']['data']
-        # subtract title from page height
-        data_height = height - 1
-        max_pages = get_max_pages(data, data_height)
-        title = format_title('system menu', width, page, max_pages)
-        n = page * data_height
-        data = data[n: n + data_height]
-        # pad page with empty rows
-        while len(data) < data_height:
-            data += ((0,) * width,)
-        set_display(tuple([title]) + tuple(data))
-    elif type(location) == int:
-        page = state['app']['books'][location]['page']
-        data = state['app']['books'][location]['data']
-        n = page * height
-        data = data[n: n + height]
-        set_display(data)
+class Display():
+    def __init__(self):
+        self.row = 0
+        self.previous_data = []
+        self.display_data = []
 
+    def render(self, state):
+        width, height = dimensions(state['app'])
+        location = state['app']['location']
+        if location == 'library':
+            page = state['app']['library']['page']
+            data = state['app']['library']['data']
+            # subtract title from page height
+            data_height = height - 1
+            max_pages = get_max_pages(data, data_height)
+            n = page * data_height
+            data = data[n: n + data_height]
+            # pad page with empty rows
+            while len(data) < data_height:
+                data += ((0,) * width,)
+            title = format_title('library menu', width, page, max_pages)
+            self.set_display(tuple([title]) + tuple(data))
+        elif location == 'menu':
+            page = state['app']['menu']['page']
+            data = state['app']['menu']['data']
+            # subtract title from page height
+            data_height = height - 1
+            max_pages = get_max_pages(data, data_height)
+            title = format_title('system menu', width, page, max_pages)
+            n = page * data_height
+            data = data[n: n + data_height]
+            # pad page with empty rows
+            while len(data) < data_height:
+                data += ((0,) * width,)
+            self.set_display(tuple([title]) + tuple(data))
+        elif type(location) == int:
+            page = state['app']['books'][location]['page']
+            data = state['app']['books'][location]['data']
+            n = page * height
+            data = data[n: n + height]
+            self.set_display(data)
 
-row = 0
-previous_data = []
-display_data = []
+    def render_line(self, driver):
+        row = self.row
+        if row >= len(self.display_data):
+            return
+        while row >= len(self.previous_data):
+            self.previous_data.append([])
+        line = self.display_data[row]
+        if line != self.previous_data[row]:
+            driver.set_braille_row(row, line)
+            self.previous_data[row] = line
+        self.row += 1
 
-
-def render_line(driver):
-    global row
-    global previous_data
-    global display_data
-    if row >= len(display_data):
-        return
-    while row >= len(previous_data):
-        previous_data.append([])
-    line = display_data[row]
-    if line != previous_data[row]:
-        driver.set_braille_row(row, line)
-        previous_data[row] = line
-    row += 1
-
-
-def set_display(data):
-    global display_data
-    global row
-    display_data = data
-    row = 0
+    def set_display(self, data):
+        self.display_data = data
+        self.row = 0
 
 
 def format_title(title, width, page_number, total_pages):
