98ce5ec025df8854f49749540603477b88204789
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-07-19 20:36:15 +0100

    Fix issue with page setting reducers

--- ui/book/reducers.py
+++ ui/book/reducers.py
@@ -4,21 +4,21 @@ from .. import utility
 class BookReducers():
     def go_to_start(self, state, value):
         width, height = utility.dimensions(state)
-        location = state['location']
-        book = state['books'][location]
+        book_n = state['book']
+        book = state['books'][book_n]
         page = 0
         books = list(state['books'])
-        books[location] = utility.set_page(book, page, height)
-        return state.copy(books=tuple(books))
+        books[book_n] = utility.set_page(book, page, height)
+        return state.copy(books=tuple(books), location='book', home_menu_visible=False)
 
     def skip_pages(self, state, value):
         width, height = utility.dimensions(state)
-        location = state['location']
-        book = state['books'][location]
+        book_n = state['location']
+        book = state['books'][book_n]
         page = book.page + value
         books = list(state['books'])
-        books[location] = utility.set_page(book, page, height)
-        return state.copy(books=tuple(books))
+        books[book_n] = utility.set_page(book, page, height)
+        return state.copy(books=tuple(books), location='book', home_menu_visible=False)
 
     def enter_go_to_page(self, state, value):
         return state.copy(home_menu_visible=False, location='go_to_page')
