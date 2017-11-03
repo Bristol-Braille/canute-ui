cc90ae8e95f9e6af3cd3165b0241ee469fb47b0f
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-11-03 19:54:56 +0000

    Remove debug from run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 while true; do
     cd /home/pi/canute-ui
-    ./canute_ui --disable-emulator --pi-buttons --debug
+    ./canute_ui --disable-emulator --pi-buttons
     sleep 1
     python3 update_ui.py
 done
