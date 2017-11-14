8eeb95c59b5111445b444861f686f98efcaf85db
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2017-11-14 16:56:14 +0000

    Convert books when being opened

--- ui/book_file.py
+++ ui/book_file.py
@@ -21,59 +21,62 @@ class BookFile():
         self.filename = filename
         self.width = width
         self.height = height
-        ext = os.path.splitext(filename)[-1].lower()
+        self.ext = os.path.splitext(filename)[-1].lower()
+        self.is_open = False
 
-        if ext == '.brf':
-            self.type = 'brf'
-            pages = []
-            page = []
-            with open(filename) as file:
-                for line in file:
-                    line = line.replace('\n', '')
-                    if FORM_FEED.match(line):
-                        # pad up to the next page and ignore this line
-                        while len(page) < height:
-                            page.append('')
-                        if line == '\f':
-                            continue
-                        else:
-                            line = line.replace('\f', '')
-                    if len(page) == height:
-                        pages.append(tuple(page))
-                        page = []
-                    page.append(braille.to_braille(line))
-            # pad the last page if it has at least one line
-            if len(page) > 0:
-                while len(page) < height:
-                    page.append('')
-                pages.append(tuple(page))
-            self.pages = tuple(pages)
+    def open(self):
+        if not self.is_open:
+            if self.ext == '.brf':
+                pages = []
+                page = []
+                with open(self.filename) as file:
+                    for line in file:
+                        line = line.replace('\n', '')
+                        if FORM_FEED.match(line):
+                            # pad up to the next page and ignore this line
+                            while len(page) < self.height:
+                                page.append('')
+                            if line == '\f':
+                                continue
+                            else:
+                                line = line.replace('\f', '')
+                        if len(page) == self.height:
+                            pages.append(tuple(page))
+                            page = []
+                        page.append(braille.to_braille(line))
+                # pad the last page if it has at least one line
+                if len(page) > 0:
+                    while len(page) < self.height:
+                        page.append('')
+                    pages.append(tuple(page))
+                self.pages = tuple(pages)
 
-        elif ext == '.pef':
-            self.type = 'pef'
-            xml_doc = minidom.parse(filename)
-            xml_pages = xml_doc.getElementsByTagName('page')
-            lines = []
-            for page in xml_pages:
-                for row in page.getElementsByTagName('row'):
-                    try:
-                        data = row.childNodes[0].data.rstrip()
-                        line = []
-                        for uni_char in data:
-                            pin_num = braille.unicode_to_pin_num(uni_char)
-                            line.append(pin_num)
-                    except IndexError:
-                        # empty row element
-                        line = tuple()
-                    lines.append(tuple(line))
-            pages = []
-            for i in range(len(lines))[::height]:
-                page = lines[i:i + height]
-                pages.append(tuple(page))
-            self.pages = tuple(pages)
+            elif self.ext == '.pef':
+                xml_doc = minidom.parse(self.filename)
+                xml_pages = xml_doc.getElementsByTagName('page')
+                lines = []
+                for page in xml_pages:
+                    for row in page.getElementsByTagName('row'):
+                        try:
+                            data = row.childNodes[0].data.rstrip()
+                            line = []
+                            for uni_char in data:
+                                pin_num = braille.unicode_to_pin_num(uni_char)
+                                line.append(pin_num)
+                        except IndexError:
+                            # empty row element
+                            line = tuple()
+                        lines.append(tuple(line))
+                pages = []
+                for i in range(len(lines))[::self.height]:
+                    page = lines[i:i + self.height]
+                    pages.append(tuple(page))
+                self.pages = tuple(pages)
+            else:
+                raise BookFileError('Unexpected extension: {}'.format(ext))
 
-        else:
-            raise BookFileError('Unexpected extension: {}'.format(ext))
+            self.is_open = True
+
 
     @property
     def title(self):
