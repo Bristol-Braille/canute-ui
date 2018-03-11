f5414a549967cf97923bcfb4c62dbee2cae2e1cb
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-03-11 00:50:03 +0000

    Clear out the system menu

--- ui/system_menu/system_menu.py
+++ ui/system_menu/system_menu.py
@@ -4,14 +4,7 @@ from ..braille import from_ascii
 from ..actions import actions
 
 system_menu = OrderedDict([
-    (
-        'replace library from USB stick',
-        actions.replace_library('start')
-    ),
     ('shutdown', actions.shutdown()),
-    ('backup log to USB stick', actions.backup_log('start')),
-    ('reset display', actions.reset_display('start')),
-    ('update system from USB stick', actions.update_ui('start')),
 ])
 
 menu_titles = tuple(map(from_ascii, system_menu))
