66f80b4fd19e8baee30a8f620655370f6bc1a77b
Canute Pi <pi@raspberrypi.local>
2018-03-15 12:30:22 +0000

    Added stty options to allow clean throughput of control characters

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+Device="/dev/ttyS0"
+
+stty -icrnl -ixon -opost -onlcr -icanon -isig -iexten -echo -echoe -echok -echoctl -echoke < "${Device}"
+
 while true; do
     cd /home/pi/canute-ui
-    ./canute_ui --real --pi-buttons --tty=/dev/ttyS0
+    ./canute_ui --real --pi-buttons --tty="${Device}"
     sleep 1
     python3 update_ui.py
 done
