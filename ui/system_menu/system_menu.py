cc1c376467ac40a0825c1d51e00a464252c04d78
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-03-15 19:40:40 +0000

    Add back select system menu options

--- ui/system_menu/system_menu.py
+++ ui/system_menu/system_menu.py
@@ -5,6 +5,7 @@ from ..actions import actions
 
 system_menu = OrderedDict([
     ('shutdown', actions.shutdown()),
+    ('backup log to USB stick', actions.backup_log('start')),
 ])
 
 menu_titles = tuple(map(from_ascii, system_menu))
