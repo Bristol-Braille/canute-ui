a85f6d09170ea495f13abdf14d84f883e0ac725c
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-04-20 21:35:47 +0100

    Fixup async_test runner

--- tests/util.py
+++ tests/util.py
@@ -4,5 +4,7 @@ import asyncio
 def async_test(coro):
     def wrapper(*args, **kwargs):
         loop = asyncio.new_event_loop()
-        return loop.run_until_complete(coro(*args, **kwargs))
+        loop.run_until_complete(coro(*args, **kwargs))
+        loop.stop()
+        loop.close()
     return wrapper
