33b775b9bb714b2560c6f62b97168df2a8a6ccdc
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-05-03 13:38:08 +0100

    Split out utility tests

--- tests/test_braille.py
+++ tests/test_braille.py
@@ -1,12 +1,5 @@
 import unittest
-import os
 import ui.braille as braille
-import ui.utility as utility
-
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-test_books_dir = os.path.join(dir_path, 'test-books')
-
 
 class TestBraille(unittest.TestCase):
     def setUp(self):
@@ -21,14 +14,6 @@ class TestBraille(unittest.TestCase):
         for p in range(64):
             self.assertEqual(braille.alpha_to_pin_num(
                 braille.pin_num_to_alpha(p)), p)
-
-    def test_find_files(self):
-        self.assertEqual(len(utility.find_files(test_books_dir, ('brf',))), 2)
-        self.assertEqual(len(utility.find_files(test_books_dir, ('pef',))), 1)
-        self.assertEqual(
-            len(utility.find_files(test_books_dir, ('brf', 'pef'))),
-            3
-        )
 
     def test_truncate_middle(self):
         inp = '123456789abcdefghi'
