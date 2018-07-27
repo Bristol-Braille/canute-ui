9ffdcc2c17b44ae5a33635412e7045c80bd89758
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2018-07-27 20:20:45 +0100

    Simplify background book loading

--- ui/book/handlers.py
+++ ui/book/handlers.py
@@ -31,7 +31,7 @@ async def init(book):
 NS = {'pef': 'http://www.daisy.org/ns/2008/pef'}
 
 
-def read_pages(book):
+async def read_pages(book, fast=False):
     if book.filename == manual.filename:
         return book
     if book.load_state == book_file.LoadState.DONE:
@@ -53,6 +53,8 @@ def read_pages(book):
                 pages.append(tuple(page))
                 page = []
             page.append(braille.from_ascii(line))
+            if not fast:
+                await asyncio.sleep(0)
         if len(page) > 0:
             # pad up to the end
             while len(page) < book.height:
@@ -66,12 +68,16 @@ def read_pages(book):
             for row in page.findall('.//pef:row', NS):
                 line = ''.join(row.itertext()).rstrip()
                 lines.append(braille.from_unicode(line))
+                if not fast:
+                    await asyncio.sleep(0)
         for i in range(len(lines))[::book.height]:
             page = lines[i:i + book.height]
             # pad up to the end
             while len(page) < book.height:
                 page.append(tuple())
             pages.append(tuple(page))
+        if not fast:
+            await asyncio.sleep(0)
     else:
         raise BookFileError(
             'Unexpected extension: {}'.format(book.ext))
@@ -88,13 +94,14 @@ async def get_page_data(book, store, page_number=None):
     if page_number is None:
         page_number = book.page_number
     if len(book.pages) == 0:
-        while book.load_state == book_file.LoadState.LOADING:
-            await asyncio.sleep(1)
-            # accessing store.state will get a fresh state
-            book = store.state['app']['user']['books'][book.filename]
+        if book.load_state == book_file.LoadState.LOADING:
+            while book.load_state == book_file.LoadState.LOADING:
+                await asyncio.sleep(0)
+                # accessing store.state will get a fresh state
+                book = store.state['app']['user']['books'][book.filename]
         else:
             await store.dispatch(actions.set_book_loading(book))
-            book = read_pages(book)
+            book = await read_pages(book, fast=True)
             await store.dispatch(actions.add_or_replace(book))
 
     if page_number >= len(book.pages):
@@ -112,48 +119,14 @@ async def fully_load_books(store):
         await store.dispatch(actions.load_books('loading'))
         books = state['user']['books']
         log.info('loading {} books'.format(len(books)))
-        aborted = False
-        futures = []
-
-        with concurrent.futures.ProcessPoolExecutor(max_workers=3) as executor:
-
-            # gather futures for reading books
-            for filename in books:
-                book = books[filename]
-                if book.load_state == book_file.LoadState.INITIAL:
-                    await store.dispatch(actions.set_book_loading(book))
-                    future = executor.submit(read_pages, book)
-                    futures.append(future)
-
-            # check if the futures are completed but don't hog this thread to wait
-            # for that, asyncio.sleep instead
-            await asyncio.sleep(2)
-            try:
-                for future in concurrent.futures.as_completed(futures, timeout=LOAD_BOOKS_TIMEOUT):
-                    # accessing store.state will get a fresh state
-                    if store.state['app']['load_books'] == 'cancel':
-                        aborted = True
-                        stop_process_pool(executor)
-                        break
-                    book = future.result(timeout=LOAD_BOOKS_TIMEOUT)
-                    await store.dispatch(actions.add_or_replace(book))
-                    await asyncio.sleep(0.1)
-            except concurrent.futures.TimeoutError:
-                log.warning('loading books timed out')
-                aborted = True
-                stop_process_pool(executor)
-
-            await store.dispatch(actions.load_books(False))
-            if aborted:
-                log.info('loading books aborted')
-            else:
-                log.info('loading books done')
-
-
-def stop_process_pool(executor):
-    for pid, process in executor._processes.items():
-        process.terminate()
-    executor.shutdown()
+        await asyncio.sleep(0.1)
+        for filename in books:
+            book = state['user']['books'][filename]
+            if book.load_state == book_file.LoadState.INITIAL:
+                await store.dispatch(actions.set_book_loading(book))
+                book = await read_pages(book)
+                await store.dispatch(actions.add_or_replace(book))
+                await asyncio.sleep(0.1)
 
 
 class BookFileError(Exception):
