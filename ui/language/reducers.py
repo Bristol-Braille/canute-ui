2c88feb70550794fd4da1578dae3d9b7490cd8ac
Lazarus404 <lee.sylvester@gmail.com>
2018-07-28 04:15:36 +0100

    Fix reducer index error

--- ui/language/reducers.py
+++ ui/language/reducers.py
@@ -4,8 +4,9 @@ from ..manual import Manual, manual_filename
 class LanguageReducers():
     def select_language(self, state, value):
         languages = state['languages']
-        lang = value
-        locale = list(languages['available'].keys())[lang]
+        lang = abs(int(value))
+        keys = list(languages['available'].keys())
+        locale = keys[lang] if lang < len(keys) else 'en_GB:en'
         manual = Manual.create(locale)
         books = state['user']['books'].copy(**{manual_filename: manual})
         user = state['user'].copy(current_language=locale, books=books)
