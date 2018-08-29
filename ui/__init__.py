3544e674d5f94a0dd3a32b4ee09853e4c4125b56
Lazarus404 <lee.sylvester@gmail.com>
2018-08-29 21:57:55 +0100

    Reset init file

--- ui/__init__.py
+++
@@ -1,9 +0,0 @@
-import locale
-import gettext
-
-
-current_locale, encoding = locale.getdefaultlocale()
-
-locale_path = 'ui/locale/'
-language = gettext.translation('canute', locale_path, [current_locale])
-language.install()
