9c3d1f7f1aa09788c19188b0b65c293416a475a6
Lazarus404 <lee.sylvester@gmail.com>
2018-08-29 21:53:37 +0100

    Simplify i18n class

--- ui/i18n.py
+++ ui/i18n.py
@@ -7,23 +7,15 @@ log = logging.getLogger(__name__)
 
 class I18n:
     def __init__(self, locale='en_GB:en'):
-        if locale:
-            self.lang = locale
-        else:
-            self.lang = None
-        self.translate = None
+        self.lang = locale
 
     def _(self, key):
-        if self.lang is not None:
-            try:
-                language = self.lang
-                translate = gettext.translation(
-                    'canute', localedir='ui/locale', languages=[language], fallback=False)
-                translate.install()
-            except Exception as e:
-                log.warning(e)
-                translate = gettext.NullTranslations()
-            self.translate = translate
-        elif not self.translate:
-            self.translate = gettext.NullTranslations()
-        return self.translate.gettext(key)
+        try:
+            language = self.lang
+            translate = gettext.translation(
+                'canute', localedir='ui/locale', languages=[language], fallback=False)
+            translate.install()
+        except Exception as e:
+            log.warning(e)
+            translate = gettext.NullTranslations()
+        return translate.gettext(key)
