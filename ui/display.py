d78e8b160eb935fa34abf579c0ed6799c61e8da7
ZakW <zw@zw>
2019-09-04 10:07:42 +0100

    Track and expose display freshness

--- ui/display.py
+++ ui/display.py
@@ -16,32 +16,31 @@ class Display():
         self.row = 0
         self.hardware_state = []
         self.buffer = []
+        self.up_to_date = True
 
     async def render_to_buffer(self, state, store):
         width, height = state_helpers.dimensions(state)
         location = state['location']
+        page_data = None
         if location == 'library':
             page_data = library_view.render(width, height, state)
-            self._set_buffer(page_data)
         elif location == 'system_menu':
             page_data = system_menu_view.render(width, height, state)
-            self._set_buffer(page_data)
         elif location == 'go_to_page':
             page_data = go_to_page_view.render(width, height, state)
-            self._set_buffer(page_data)
         elif location == 'language':
             page_data = language_view.render(width, height, state)
-            self._set_buffer(page_data)
         elif location == 'book':
             page_data = await book_view.render(width, height, state, store)
-            self._set_buffer(page_data)
         elif location == 'bookmarks_menu':
             page_data = await bookmarks_view.render(width, height, state, store)
+        if page_data:
             self._set_buffer(page_data)
 
     async def send_line(self, driver):
         row = self.row
         if row >= len(self.buffer):
+            self.up_to_date = True
             return
         self.row += 1
         while row >= len(self.hardware_state):
@@ -51,6 +50,11 @@ class Display():
             await driver.async_set_braille_row(row, braille)
             self.hardware_state[row] = braille
 
+    def is_up_to_date(self):
+        return self.up_to_date
+
     def _set_buffer(self, data):
-        self.buffer = data
+        if data != self.buffer:
+            self.buffer = data
+            self.up_to_date = False
         self.row = 0
