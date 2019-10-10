99cbddc0fcf86dcba988f80ebfbdfbec8917a10a
ZakW <zw@zw>
2019-10-10 12:22:24 +0100

    Document bookmark deletion in help
    Fixes #212.

--- ui/bookmarks/help.py
+++ ui/bookmarks/help.py
@@ -9,7 +9,10 @@ To navigate to a bookmark within a file press the line select button \
 immediately to the left of your chosen bookmark. From the bookmark \
 menu you can also navigate to the beginning of your book by selecting \
 "start of book", or the end of a book by selecting "end of book", \
-using the line select buttons.\
+using the line select buttons. You can delete a bookmark by pressing \
+and holding the line select button to the left of the bookmark you \
+wish to delete for three seconds. You cannot delete the "start of \
+book" or "end of book" bookmarks.\
 ''')
 
     for line in para.split('\n'):
