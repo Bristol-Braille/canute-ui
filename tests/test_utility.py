21cbae53eecb788649105b64d1ba71d38884f544
Andy Wood <andy@bitshaper.co.uk>
2023-09-15 17:09:51 +0100

    Restore library test

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,17 +1,17 @@
 import unittest
 import os
 
-import ui.utility as utility
+from ui.library.explorer import Library
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-test_books_dir = os.path.join(dir_path, 'test-books')
+test_books_dir = [('test-books', 'test dir')]
 
 
 class TestUtility(unittest.TestCase):
     def test_find_files(self):
-        self.assertEqual(len(utility.find_files(test_books_dir, ('brf',))), 2)
-        self.assertEqual(len(utility.find_files(test_books_dir, ('pef',))), 1)
-        self.assertEqual(
-            len(utility.find_files(test_books_dir, ('brf', 'pef'))),
-            3
-        )
+        library = Library(dir_path, test_books_dir, ('brf',))
+        self.assertEqual(len(library.book_files()), 2)
+        library = Library(dir_path, test_books_dir, ('pef',))
+        self.assertEqual(len(library.book_files()), 1)
+        library = Library(dir_path, test_books_dir, ('brf', 'pef'))
+        self.assertEqual(len(library.book_files()), 3)
