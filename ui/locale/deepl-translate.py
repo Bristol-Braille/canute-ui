15a4194432e1185994b15ac777e7d2763204c82e
Andy Wood <andy@bitshaper.co.uk>
2025-09-19 11:57:35 +0100

    Try to make the script work with new translations as well as old ones

--- ui/locale/deepl-translate.py
+++ ui/locale/deepl-translate.py
@@ -12,29 +12,30 @@ import time
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Translate .po files using DeepL")
-parser.add_argument("-f", "--file", required=True, help="Path to the .po file")
+parser.add_argument("-i", "--input", required=True, help="Path to the .po file")
+parser.add_argument("-o", "--output", required=True, help="Path to the translated .po file")
 parser.add_argument("-l", "--lang", required=True, help="Target language code")
 args = parser.parse_args()
 
-po_file = args.file
 lang = args.lang.upper()  # Convert to uppercase
 
 # DeepL API Key
 AUTH_KEY = os.environ.get('DEEPL_API_KEY') 
 translator = deepl.Translator(AUTH_KEY)
 
-# Only translate non braille translations - 0x2800-0x283F is 6-dot braille 
-def should_translate(msgstr):
-    return all(ord(c) < 0x2800 for c in msgstr)
+# Only translate non braille translations - 0x2800-0x283F is 6-dot braille
+# or any that have been fuzzy matched by msgmerge
+def should_translate(entry):
+    return 'fuzzy' in entry.flags or all(ord(c) < 0x2800 for c in entry.msgstr)
 
 # Load the .po file
-po = polib.pofile(po_file)
+po = polib.pofile(args.input)
 total_entries = len([entry for entry in po if entry.msgid and should_translate(entry.msgstr)])
 translated_count = 0
 
 # Translate entries with progress
 for i, entry in enumerate(po):
-    if entry.msgid and should_translate(entry.msgstr):  
+    if entry.msgid and should_translate(entry):  
         try:
             translation = translator.translate_text(entry.msgid, target_lang=lang)
             entry.msgstr = translation.text
@@ -46,7 +47,6 @@ for i, entry in enumerate(po):
             print(f"\nError translating '{entry.msgid}': {e}")
 
 # Save the translated .po file
-translated_file = po_file.replace(".po", f"_translated_{lang}.po")
-po.save(translated_file)
+po.save(args.output)
 
-print(f"\nTranslation completed: {translated_file}") 
+print(f"\nTranslation completed: {args.output}") 
--- ui/locale/liblouis-transcribe.py
+++ ui/locale/liblouis-transcribe.py
@@ -1,8 +1,6 @@
 
-import os
 import argparse
 import polib
-import time
 import louis
 import textwrap
 from datetime import datetime, timezone
@@ -11,68 +9,74 @@ CANUTE_WIDTH = 40
 
 # Parse command-line arguments
 parser = argparse.ArgumentParser(description="Translate .po files using liblouis")
-parser.add_argument("-f", "--file", required=True, help="Path to the language .po file")
+parser.add_argument("-i", "--input", required=True, help="Path to the input language .po file")
+parser.add_argument("-o", "--output", required=True, help="Path to the output braille .po file")
 parser.add_argument("-t", "--table", required=True, help="Target braille table")
 parser.add_argument("-n", "--name", required=True, help="Language and table name to use in menu")
 args = parser.parse_args()
 
-po_file = args.file
-src = polib.pofile(po_file)
-valid_entries = [e for e in src if not e.obsolete]
+MENU_MSGID = 'Language Name, UEB grade'
 
-dest = polib.POFile()
+# Only translate non braille translations - 0x2800-0x283F is 6-dot braille
+# or any that have been fuzzy matched by msgmerge
+def should_translate(entry):
+    return 'fuzzy' in entry.flags or all(ord(c) < 0x2800 for c in entry.msgstr)
+
+dest = polib.pofile(args.output)
+src = polib.pofile(args.input)
+valid_entries = [e for e in src if not e.obsolete]
 
 dest.metadata = src.metadata
 now = datetime.now(timezone.utc).strftime('%F %H:%M%z')
 dest.metadata['PO-Revision-Date'] = now
 
 for src_entry in valid_entries:
-    # we use double spaces (from \n + \n) as line break indicator
-    if src_entry.msgid.find('  ') != -1:
-        print('Warning: embedded double-space:\n' +
-                str(src_entry.occurrences))
-    if src_entry.msgid == 'Language Name, UEB grade':
-        msgstr = args.name
-    else:
-        msgstr = src_entry.msgstr
 
-    translation = louis.translateString(
-        ['unicode.dis', args.table],
-        msgstr
-    )
+    dest_entry = dest.find(src_entry.msgid)
 
-    # normalise to use ascii spaces for textwrap's benefit
-    # note the first parameter below is a braille space!
-    translation = translation.replace('⠀', ' ')
+    if should_translate(dest_entry):
+        # we use double spaces (from \n + \n) as line break indicator
+        if src_entry.msgid.find('  ') != -1:
+            print('Warning: embedded double-space:\n' + str(src_entry.occurrences))
 
-    # Louis squashes newlines into spaces.  Provided there are
-    # no double-spaces in the input we can assume a double-space
-    # in the output was meant to be a line break.
-    import re
-    wrapped = []
-    for para in re.split(r'  ', translation):
-        wrapped.append(textwrap.fill(para, width=CANUTE_WIDTH))
-        wrapped.append('\n')
-        wrapped.append('\n')
-    wrapped.pop()
-    wrapped.pop()
-    # At this point we could apply a general rule that no page
-    # should ever start with a blank line; would save a manual tweak
-    # or two.
-    wrapped = ''.join(wrapped)
-    # convert back to braille spaces
-    wrapped = wrapped.replace(' ', '⠀')
-    dest_entry = polib.POEntry(
-        msgid=src_entry.msgid,
-    )
-    dest_entry.merge(src_entry)
-    dest_entry.msgstr = wrapped
-    dest.append(dest_entry)
+        if src_entry.msgid == MENU_MSGID:
+            msgstr = args.name
+        else:
+            msgstr = src_entry.msgstr
 
-destdir = os.path.dirname(po_file)
-po_path = os.path.join(destdir, 'canute.po')
-dest.save(po_path)
-print(po_path)
-mo_path = os.path.join(destdir, 'canute.mo')
-dest.save_as_mofile(mo_path)
-print(mo_path)
+        translation = louis.translateString(
+            ['unicode.dis', args.table],
+            msgstr
+        )
+
+        # normalise to use ascii spaces for textwrap's benefit
+        # note the first parameter below is a braille space!
+        translation = translation.replace('⠀', ' ')
+
+        # Louis squashes newlines into spaces.  Provided there are
+        # no double-spaces in the input we can assume a double-space
+        # in the output was meant to be a line break.
+        import re
+        wrapped = []
+        for para in re.split(r'  ', translation):
+            wrapped.append(textwrap.fill(para, width=CANUTE_WIDTH))
+            wrapped.append('\n')
+            wrapped.append('\n')
+        wrapped.pop()
+        wrapped.pop()
+        # At this point we could apply a general rule that no page
+        # should ever start with a blank line; would save a manual tweak
+        # or two.
+        wrapped = ''.join(wrapped)
+        # convert back to braille spaces
+        wrapped = wrapped.replace(' ', '⠀')
+        dest_entry = polib.POEntry(
+            msgid=src_entry.msgid,
+        )
+        dest_entry.merge(src_entry)
+        dest_entry.msgstr = wrapped
+
+        if 'fuzzy' in dest_entry.flags and 'fuzzy' not in src_entry.flags:
+            dest_entry.flags.remove('fuzzy')
+
+dest.save(args.ouput)
