d33e00f85ed4eb670885d480f7478a1e4e357a33
Andy Wood <andy@bitshaper.co.uk>
2025-11-25 16:58:06 +0000

    Automated Russian translation

--- ui/locale/update-all.sh
+++ ui/locale/update-all.sh
@@ -10,3 +10,4 @@ cd "${locale_dir}"
 ./update-language.sh de_DE DE de-g1-detailed.ctb 'Deutsch, G1' de-g2.ctb 'Deutsch, G2'
 ./update-language.sh fr_FR FR fr-bfu-comp6.utb 'Français, G1' fr-bfu-g2.ctb 'Français, G2'
 ./update-language.sh mn_MN MN mn-MN-g1.utb 'Монгол, G1' mn-MN-g2.ctb 'Монгол, G2'
+./update-language.sh ru_RU RU ru-litbrl-detailed.utb 'русский'
